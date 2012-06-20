7f96e033364a110c89f73865081a398e18ec4457
Michael Kohler <michaelkohler@live.com>
2012-06-21 01:06:43 +0200

    GUIHelper and ScrollableTable added to GUI package

---
+++ src/main/info/michaelkohler/helpertools/gui/GUIHelper.java
@@ -0,0 +1,108 @@
+/* ============== HelperTools ==============
+ * Initial developer: Michael Kohler <michaelkohler@live.com>
+ *
+ * =====
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE - Version 2
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ * TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ * 0. You just DO WHAT THE FUCK YOU WANT TO.
+ *
+ * =====
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+ *
+ */
+
+package info.michaelkohler.helpertools.gui;
+
+import info.michaelkohler.helpertools.logging.Debugger;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.KeyStroke;
+import javax.swing.UIManager;
+
+/**
+ * GUI Helper for the project. It makes different helper methods available
+ * to the other classes.
+ * 
+ * @author Michael Kohler <michaelkohler@linux.com>
+ * @version 0.0.1
+ *
+ */
+public class GUIHelper {
+
+    /**
+     * makes the component visible.
+     * 
+     * @param aComponent which component should be visible
+     */
+    public static void showComponent(Component aComponent) {
+        aComponent.setVisible(true);
+    }
+
+    /**
+     * makes the component invisible.
+     * 
+     * @param aComponent which component should be invisible
+     */
+    public static void hideComponent(Component aComponent) {
+        aComponent.setVisible(false);
+    }
+    
+    /**
+     * makes the application pretty using the standard OS design.
+     * 
+     */
+    public static void prettify() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            Debugger.logMessage(ex);
+        }
+    }
+
+    /**
+     * sizes the JFrame
+     * 
+     * @param aFrame which needs to be sized
+     * @param aWidth defining the width of the window
+     * @param aHeight defining the height of the window
+     */
+    public static void sizeAndCenterFrame(JFrame aFrame, int aWidth, int aHeight) {
+        aFrame.setSize(aWidth, aHeight);
+        aFrame.setLocationRelativeTo(null);
+    }
+
+    /**
+     * makes the JFrame closeable with ESC key.
+     * 
+     * @param aFrame which needs to be closeable with the ESC key
+     */
+    @SuppressWarnings("serial")
+    public static void setESCCloseable(final JFrame aFrame) {
+        KeyStroke escapeKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0,
+                                                                                  false);
+        Action escapeAction = new AbstractAction() {
+            public void actionPerformed(ActionEvent ae) {
+                aFrame.dispose();
+            }
+        }; 
+        aFrame.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
+                  .put(escapeKeyStroke, "ESCAPE");
+        aFrame.getRootPane().getActionMap().put("ESCAPE", escapeAction);
+        
+    }
+
+}
---
+++ src/main/info/michaelkohler/helpertools/gui/ScrollableTable.java
@@ -0,0 +1,78 @@
+/* ============== HelperTools ==============
+ * Initial developer: Michael Kohler <michaelkohler@live.com>
+ *
+ * =====
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE - Version 2
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ * TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ * 0. You just DO WHAT THE FUCK YOU WANT TO.
+ *
+ * =====
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+ *
+ */
+
+package info.michaelkohler.helpertools.gui;
+
+import java.awt.Color;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+
+/**
+ * The ScrollableTable is responsible to produce a scrollable table with
+ * the given data input. It automatically adds a JTable to a JScrollPane
+ * which then can be used further.
+ * 
+ * @author Michael Kohler <michaelkohler@linux.com>
+ * @version 0.0.1
+ *
+ */
+
+@SuppressWarnings("serial")
+public class ScrollableTable extends JScrollPane {
+
+    /**
+     * Constructor which initializes the component.
+     * 
+     * @param aData specifying the data which should be displayed in the table
+     * @param aColumnNames specifying the column names
+     */
+    public ScrollableTable(String[][] aData, String[] aColumnNames) {
+        super();
+
+        TableModel model = getTableModel(aData, aColumnNames);
+        JTable overviewTable = new JTable(model);
+        overviewTable.setGridColor(new Color(240, 240, 240));
+        TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(model);
+        overviewTable.setRowSorter(sorter);
+
+        this.setViewportView(overviewTable);
+    }
+    
+    /**
+     * Produces the TableModel which is used by the table. This is a standard
+     * model.
+     * 
+     * @param aData specifying the data which should be displayed in the table
+     * @param aColumnNames specifying the column names
+     * @return model which was produced
+     */
+    private TableModel getTableModel(String[][] aData, String[] aColumnNames) {
+        TableModel model = new DefaultTableModel(aData, aColumnNames) {
+            public Class<?> getColumnClass(int column) {
+                return getValueAt(0, column).getClass();
+            }
+        };
+        return model;
+    }
+}
