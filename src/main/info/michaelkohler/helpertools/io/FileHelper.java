6634089f7b43052ae9da5addbdb0e66af2c3c0a6
Mark Veronda <mark.veronda@gmail.com>
2012-12-01 01:55:11 -0800

    Added assert error for private constructor as missed it from the previous commit

--- src/main/info/michaelkohler/helpertools/io/FileHelper.java
+++ src/main/info/michaelkohler/helpertools/io/FileHelper.java
@@ -42,7 +42,7 @@ public final class FileHelper {
      * Empty private constructor, no instantiation needed.
      */
     private FileHelper() {
-
+    	throw new AssertionError("Cannot instantiate this class");
     }
 
     /**
