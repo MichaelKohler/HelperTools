13fe0bf2c480b857eb9628c39849715f1c489bbd
ivan.sim <ivan.sim@aeroinfo.com>
2012-12-14 11:14:09 -0800

    Promote 'today', 'yesterday' and 'tomorrow' strings to class constants.

--- src/main/info/michaelkohler/helpertools/date/DateHelper.java
+++ src/main/info/michaelkohler/helpertools/date/DateHelper.java
@@ -10,21 +10,24 @@ import java.util.Locale;
 public class DateHelper {
   private static final Locale DEFAULT_LOCALE = Locale.ENGLISH;
   private static final String SIMPLE_DATE_FORMAT = "yyyy/MM/dd";
+  private static final String TOMORROW = "tomorrow";
+  private static final String YESTERDAY = "yesterday";
+  private static final String TODAY = "today";
 
   /**
    * Convert string to date
-   * @param dateStr String to be converted.
+   * @param dateStr String to be converted. Supported constants: today, yesterday, tomorrow.
    * @return A {@link Date} object representation of the given string.
    */
   public static Date strToDate(String dateStr) {
-    if(dateStr.equalsIgnoreCase("today"))
+    if(dateStr.equalsIgnoreCase(TODAY))
       return new Date();
-    else if(dateStr.equalsIgnoreCase("yesterday")) {
+    else if(dateStr.equalsIgnoreCase(YESTERDAY)) {
       Calendar cal = new GregorianCalendar();
       cal.roll(Calendar.DATE, false);
       return cal.getTime();
     }
-    else if(dateStr.equalsIgnoreCase("tomorrow")) {
+    else if(dateStr.equalsIgnoreCase(TOMORROW)) {
       Calendar cal = new GregorianCalendar();
       cal.roll(Calendar.DATE, true);
       return cal.getTime();
