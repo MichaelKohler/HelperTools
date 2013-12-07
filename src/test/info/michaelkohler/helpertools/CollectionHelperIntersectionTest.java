11fb43d6a9038b2aa154b37af5f0feddd0d53884
isim <ihcsim@gmail.com>
2013-12-07 00:04:29 -0800

    More tests for the CollectionHelper.intersect() method for string collections.

--- src/test/info/michaelkohler/helpertools/CollectionHelperIntersectionTest.java
+++ src/test/info/michaelkohler/helpertools/CollectionHelperIntersectionTest.java
@@ -241,5 +241,95 @@ public class CollectionHelperIntersectionTest {
     for(Integer firstListItem : listOne)
       Assert.assertTrue(actual.contains(firstListItem));
   }
-
+  
+  @Test
+  public void testIntersectionOfTwoLists_ExcludedElements_IntegerType() {
+    List<Integer> firstNamesList = new ArrayList<Integer>();
+    firstNamesList.add(1); firstNamesList.add(2); firstNamesList.add(3);
+    firstNamesList.add(4); firstNamesList.add(5);
+    
+    List<Integer> secondNamesList = new ArrayList<Integer>();
+    secondNamesList.add(1); secondNamesList.add(2); secondNamesList.add(3);
+    
+    List<Integer> expectedExcluded = new ArrayList<Integer>();
+    expectedExcluded.add(4); expectedExcluded.add(5);
+    
+    Collection<Integer> actual = CollectionHelper.intersect(firstNamesList, secondNamesList);
+    Assert.assertEquals(3, actual.size());
+    for(Integer excluded : expectedExcluded)
+      Assert.assertTrue(!actual.contains(excluded));
+  }
+  
+  @Test
+  public void testIntersectionOfTwoLists_StringComparison_Test1() {
+    List<String> firstNamesList = new ArrayList<String>();
+    firstNamesList.add("John"); firstNamesList.add("Mike"); firstNamesList.add("Chris");
+    firstNamesList.add("Matt"); firstNamesList.add("Bill");
+    
+    List<String> secondNamesList = new ArrayList<String>();
+    secondNamesList.add("John"); secondNamesList.add("Mike"); secondNamesList.add("Peter");
+    secondNamesList.add("David"); secondNamesList.add("Matthew");
+    
+    List<String> expected = new ArrayList<String>();
+    expected.add("John"); expected.add("Mike");
+    
+    Collection<String> actual = CollectionHelper.intersect(firstNamesList, secondNamesList);
+    Assert.assertEquals(expected.size(), actual.size());
+    for(String expectedItem : expected)
+      Assert.assertTrue(actual.contains(expectedItem));
+  }
+  
+  @Test
+  public void testIntersectionOfTwoLists_StringComparison_Test2() {
+    List<String> firstNamesList = new ArrayList<String>();
+    firstNamesList.add("Sherry"); firstNamesList.add("Liz"); firstNamesList.add("Mary");
+    firstNamesList.add("Jenny"); firstNamesList.add("Christine");
+    
+    List<String> secondNamesList = new ArrayList<String>();
+    secondNamesList.add("Sherry"); secondNamesList.add("Liz"); secondNamesList.add("Mary");
+    
+    List<String> expected = new ArrayList<String>();
+    expected.add("Sherry"); expected.add("Liz"); expected.add("Mary");
+    
+    Collection<String> actual = CollectionHelper.intersect(firstNamesList, secondNamesList);
+    Assert.assertEquals(expected.size(), actual.size());
+    for(String expectedItem : expected)
+      Assert.assertTrue(actual.contains(expectedItem));
+  }
+  
+  @Test
+  public void testIntersectionOfTwoLists_StringComparison_NoDuplicates() {
+    List<String> firstNamesList = new ArrayList<String>();
+    firstNamesList.add("Hansen"); firstNamesList.add("Lisa"); firstNamesList.add("Elizabeth");
+    firstNamesList.add("Cassy"); firstNamesList.add("Alex");
+    
+    List<String> secondNamesList = new ArrayList<String>();
+    secondNamesList.add("Hansen");
+    
+    List<String> expected = new ArrayList<String>();
+    expected.add("Hansen");
+    
+    Collection<String> actual = CollectionHelper.intersect(firstNamesList, secondNamesList);
+    Assert.assertEquals(expected.size(), actual.size());
+    for(String expectedItem : expected)
+      Assert.assertTrue(actual.contains(expectedItem));
+  }
+  
+  @Test
+  public void testIntersectionOfTwoLists_ExcludedElements_StringType() {
+    List<String> firstNamesList = new ArrayList<String>();
+    firstNamesList.add("Hansen"); firstNamesList.add("Lisa"); firstNamesList.add("Elizabeth");
+    firstNamesList.add("Cassy"); firstNamesList.add("Alex");
+    
+    List<String> secondNamesList = new ArrayList<String>();
+    secondNamesList.add("Hansen"); secondNamesList.add("Lisa"); secondNamesList.add("Elizabeth");
+    
+    List<String> expectedExcluded = new ArrayList<String>();
+    expectedExcluded.add("Cassy"); expectedExcluded.add("Alex");
+    
+    Collection<String> actual = CollectionHelper.intersect(firstNamesList, secondNamesList);
+    Assert.assertEquals(3, actual.size());
+    for(String excluded : expectedExcluded)
+      Assert.assertTrue(!actual.contains(excluded));
+  }
 }
